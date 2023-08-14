fadcb0cb71e7616e6c8c7332a073ed0937b389c5
Tejas Srinivas <stejas2002@gmail.com>
2023-08-15 00:53:18 +0530

    updated directory

---
+++ server/routes/route.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { addTodo , getAllTodos , toggleTodoDone , updateTodo , deleteTodo } from "../../../controller/todo-controller.js";
+// import { getAllTodos } from "../../client/src/redux/actions/index.js";
+
+const route = express.Router();
+
+//it will call the server function such as addTodo,getAlltodos etc
+route.post('/todos', addTodo)
+route.get('/todos',getAllTodos)
+route.get('/todos/:id',toggleTodoDone)
+route.put('/todos/:id',updateTodo)
+route.delete('/todos/:id',deleteTodo)
+
+export default route
